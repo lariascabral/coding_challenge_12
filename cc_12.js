be4b18f6bfb7ba871635e664a28008067e6ad1e5
Lisbeth Arias Cabral <lariascabral@usf.edu>
2025-03-02 20:58:08 -0500

    Task 1 -  Commentary

--- cc_12.js
+++ cc_12.js
@@ -1,15 +1,15 @@
 // Task 1 - Created Revenue Metric Card
 
 document.addEventListener("DOMContentLoaded", () => {
-    const dashboardById = document.getElementById("dashboard") ;
-    const dashboardByQuery = document.querySelector("#dasboard") ;
+    const dashboardById = document.getElementById("dashboard") ;                // Using getElementById to obtain the dashboard container
+    const dashboardByQuery = document.querySelector("#dashboard") ;             // Using querySelector to obtain the dashboard container
 
-    console.log(dashboardById === dashboardByQuery) ;
+    console.log(dashboardById === dashboardByQuery) ;                           // Making sure that the slections are the same element, console shows true
 
-    const revenueCard = document.createElement("div") ;
+    const revenueCard = document.createElement("div") ;                         // Establishing a div element for the RevenueCard
     revenueCard.setAttribute("class", "metric-card") ;
     revenueCard.innerHTML = "<h3>Revenue</h3><p>$0</p>" ;
 
-    dashboardById.appendChild(revenueCard) ;
-
+    dashboardById.appendChild(revenueCard) ;                                    // Appending the selection --- coudl use also querySelector since referencing the same
 }) ;
+
